35e31fc42fe2ccf015d4014adf78a3daa8f5cbd8
dwb <donwiktorb@gmail.com>
2023-11-15 12:07:31 +0100

    fix(main): youtube for some reasons removed the functions from global so yeah

--- main.js
+++ main.js
@@ -1,114 +1,41 @@
 (async() => {
-        
-        function getFullFunctionName(name) {
-            let found = false
+        // if (true)
+        //     return console.log('YOUTUBE CHANGED SOME STUFF SO WORKING ON FIX') 
+        const initObserve = (selector, cb) => {
+            let startElem = document.querySelector(selector)
 
-            for (let i in window) {
-                if (i.startsWith(name)) {
-                    found = i
+            if (startElem) {
+                try {
+                    cb(startElem)
+                } catch(e) {
+                    console.log(e)
                 }
             }
 
-            return found
+            const observer = new MutationObserver(mutations => {
+                let elem = document.querySelector(selector)
+                if (elem) {
+                    try {
+                        cb(elem)
+                    } catch(e) {
+                        console.log(e)
+                    }
+                }
+            });
+
+            observer.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
         }
 
         try {
-            console.log('waiting for youtube ads to be loaded...')
-
-            do {
-                await new Promise(r => setTimeout(r, 200));
-                console.log('waiting')
-
-            } while(getFullFunctionName('ytPlayeronAdStateChangeplayer') == false)
-
-            console.log('loaded')
-
-            let name = getFullFunctionName('ytPlayeronAdStateChangeplayer')
-
-            let func = window[name]
-
-            console.log(func, name)
-
-            window.currentVideoTime = 0
-            window.showingAd = false
-            window.lastUrl = false
-
-            window[name] = async (s) => {
-                console.log("AD STATE CHANGE", s)
-                let videoElem = document.getElementById('movie_player')
-
-                if (s === -1 || s == 1) {
-                    let skipElem = document.getElementsByClassName("ytp-ad-skip-button-container")
-                    if (skipElem && skipElem[0])
-                        return skipElem[0]?.click()
-
-                    window.showingAd = true
-                    videoElem.stopVideo()
-
-                    if (window.lastUrl) {
-                        if (window.lastUrl !== videoElem.getVideoUrl()) {
-                            window.lastUrl = videoElem.getVideoUrl()
-                            window.currentVideoTime = 0
-                        }
-                    } else {
-                        window.lastUrl = videoElem.getVideoUrl()
-                    }
-                    do {
-                        await new Promise(r => setTimeout(r, 200));
-                        videoElem.seekTo(window.currentVideoTime)
-                    } while(videoElem.getCurrentTime() !== window.currentVideoTime)
-
-                } else {
-                    window.showingAd = false
-                }
-            }
-
-            do {
-                await new Promise(r => setTimeout(r, 200));
-                console.log('waiting for progress update player')
-
-            } while(getFullFunctionName('ytPlayeronVideoProgressplayer') == false)
-
-            let progressName = getFullFunctionName('ytPlayeronVideoProgressplayer')
-
-            let func2 = window[progressName]
-
-            window[progressName] = (t) => {
-                if (t > 5) {
-                    console.log("TIME UPDATE", t)
-                    window.currentVideoTime = t
-                }
-
-                if (func2)
-                    func2(t)
-            }
-
-            do {
-                await new Promise(r => setTimeout(r, 200));
-                console.log('waiting for player state change')
-
-            } while(getFullFunctionName('ytPlayeronStateChangeplayer') == false)
-
-            let stateName = getFullFunctionName('ytPlayeronStateChangeplayer')
-
-            let func4 = window[stateName]
-
-            window[stateName] = (s) => {
-                console.log("PLAYER STATE CHANGE ", s)
-                // if (s === 2) // 0
-                //     window.currentVideoTime = 0
-                // if (s === 1) {
-                //     let videoElem = document.getElementById('movie_player')
-                //     if (window.lastUrl !== videoElem.getVideoUrl()) {
-                //         window.lastUrl = videoElem.getVideoUrl()
-                //         window.currentVideoTime = 0
-                //     }
-                // }
-                if (func4)
-                    func4(s)
-            }
-            
+            initObserve('.ytp-ad-skip-button-container', (elem) => {
+                elem.click()
+                console.log('skipped')
+            })
+            console.log("SET")
         } catch(e) {
-            console.log(e)
+            console.log("Error ", e)
         }
 })()
