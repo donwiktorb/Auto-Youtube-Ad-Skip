fdaee99884f11bbf2a840f0119d15d818054beea
donwiktorb <email@exmaple.com>
2024-06-01 18:18:58 +0200

    youtube updated, so did i

--- main.js
+++ main.js
@@ -1,50 +1,51 @@
-(async() => {
-        // if (true)
-        //     return console.log('YOUTUBE CHANGED SOME STUFF SO WORKING ON FIX') 
-        const initObserve = (selector, cb) => {
-            let startElem = document.querySelector(selector)
+(async () => {
+  // if (true)
+  //     return console.log('YOUTUBE CHANGED SOME STUFF SO WORKING ON FIX')
+  const initObserve = (selector, cb) => {
+    let startElem = document.querySelector(selector);
 
-            if (startElem) {
-                try {
-                    cb(startElem)
-                } catch(e) {
-                    console.log(e)
-                }
-            }
+    if (startElem) {
+      try {
+        cb(startElem);
+      } catch (e) {
+        console.log(e);
+      }
+    }
 
-            const observer = new MutationObserver(mutations => {
-                let elem = document.querySelector(selector)
-                if (elem) {
-                    try {
-                        cb(elem)
-                    } catch(e) {
-                        console.log(e)
-                    }
-                }
-            });
-
-            observer.observe(document.body, {
-                childList: true,
-                subtree: true
-            });
-        }
-
+    const observer = new MutationObserver((mutations) => {
+      let elem = document.querySelector(selector);
+      if (elem) {
         try {
-            initObserve('.ytp-ad-skip-button-container', (elem) => {
-                elem.click()
-                console.log('skipped')
-            })
-            initObserve('.ytp-ad-preview-container', (elem) => {
-                let btnElem = document.querySelector('.ytp-ad-skip-button-container')
-                if (!btnElem) {
-                    let videoElem = document.getElementsByTagName('video')
-                    if (videoElem) videoElem = videoElem[0]
-                    videoElem?.fastSeek(videoElem.duration)
-                    console.log('skipped')
-                }
-            })
-            console.log("SET")
-        } catch(e) {
-            console.log("Error ", e)
+          cb(elem);
+        } catch (e) {
+          console.log(e);
         }
-})()
+      }
+    });
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  };
+
+  try {
+    initObserve(".ytp-ad-skip-button-container", (elem) => {
+      elem.click();
+      console.log("skipped");
+    });
+    initObserve(" ytp-ad-player-overlay-layout ", (elem) => {
+      let btnElem = document.querySelector(".ytp-ad-skip-button-container");
+      if (!btnElem) {
+        let videoElem = document.getElementsByTagName("video");
+        if (videoElem) videoElem = videoElem[0];
+        videoElem?.fastSeek(videoElem.duration ?? 4444);
+        console.log("skipped");
+      }
+    });
+    console.log("SET");
+  } catch (e) {
+    console.log("Error ", e);
+  }
+})();
+
